2d450207ab9023f39880b2214433d6705288caf7
Soichi Hayashi <hayashis@iu.edu>
2020-01-24 16:24:48 +0000

    updated to match the new neuro/surface datatype structure

--- convertsurf.sh
+++ convertsurf.sh
@@ -2,12 +2,12 @@
 
 surf=fmripout/freesurfer/sub-TTTEMPSUB/surf
 
-mris_convert $surf/lh.pial surface/L.pial.surf.gii
-mris_convert $surf/rh.pial surface/R.pial.surf.gii
+mris_convert $surf/lh.pial surface_left/pial.surf.gii
+mris_convert $surf/rh.pial surface_right/pial.surf.gii
 
-mris_convert $surf/lh.white surface/L.white.surf.gii
-mris_convert $surf/rh.white surface/R.white.surf.gii
+mris_convert $surf/lh.white surface_left/white.surf.gii
+mris_convert $surf/rh.white surface_right/white.surf.gii
 
-mris_convert $surf/lh.inflated surface/L.inflated.surf.gii
-mris_convert $surf/rh.inflated surface/R.inflated.surf.gii
+mris_convert $surf/lh.inflated surface_left/inflated.surf.gii
+mris_convert $surf/rh.inflated surface_right/inflated.surf.gii
 
