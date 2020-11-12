83d51fe340169281dc106919be2e87099242a19e
Soichi Hayashi <hayashis@iu.edu>
2020-11-12 19:24:11 +0000

    updated to output in new surface datatypes

--- convertsurf.sh
+++ convertsurf.sh
@@ -3,12 +3,12 @@
 surf=$1
 echo "converting surfaces to .gii in $surf"
 
-mris_convert $surf/lh.pial surface/left_pial.surf.gii
-mris_convert $surf/rh.pial surface/right_pial.surf.gii
+mris_convert $surf/lh.pial surface-vertices/left/pial.gii
+mris_convert $surf/rh.pial surface-vertices/right/pial.gii
 
-mris_convert $surf/lh.white surface/left_white.surf.gii
-mris_convert $surf/rh.white surface/right_white.surf.gii
+mris_convert $surf/lh.white surface-vertices/left/white.gii
+mris_convert $surf/rh.white surface-vertices/right/white.gii
 
-mris_convert $surf/lh.inflated surface/left_inflated.surf.gii
-mris_convert $surf/rh.inflated surface/right_inflated.surf.gii
+mris_convert $surf/lh.inflated surface-vertices/left/inflated.gii
+mris_convert $surf/rh.inflated surface-vertices/right/inflated.gii
 
