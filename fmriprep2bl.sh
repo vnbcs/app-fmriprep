85fe174d32c39fe3138071df04d9b12bc9225a37
Brad Caron <bacaron@iu.edu>
2022-05-19 13:32:36 -0400

    Update fmriprep2bl.sh

--- fmriprep2bl.sh
+++ fmriprep2bl.sh
@@ -103,6 +103,9 @@ ln -sf ../$(find $oDir/anat -name "*_desc-aparcaseg_dseg.nii.gz" -not -name "*sp
 ### transform/h5 datatype
 mkdir -p transform
 #oDir=$outdir/fmriprep/sub-$sub
+if [[ ${space} == 'T1w' ]]; then
+    space="fsnative"
+fi
 ln -sf ../$(find $oDir/anat -name "*_from-T1w_to-${space}_mode-image_xfm.h5") transform/warp.h5
 ln -sf ../$(find $oDir/anat -name "*_from-${space}_to-T1w_mode-image_xfm.h5") transform/inverse-warp.h5
 ln -sf ../$(find $oDir/anat -name "*_from-T1w_to-*mode-image_xfm.txt") transform/affine.txt
